9e362d409365b9d044a42ede9e6e372e30ea1657
pg598595 <pg598595@gmail.com>
2022-10-31 10:12:05 -0400

    [done]

--- src/routes/note.js
+++ src/routes/note.js
@@ -20,45 +20,45 @@ router.post('/', async (req, res) => {
   const newText = req.body.text;
 
   let query = "INSERT INTO notes5 (text) VALUES ('" +
-  newText + "')";
+    newText + "')";
 
   let queryNewNote = "SELECT * FROM notes5 ORDER BY id desc LIMIT 1";
 
   var newNote = {} // this is the response object, make sure to replace with actual value
 
- 
+
 
   // execute query
   await db.query(query, (err, result) => {
 
     console.log(result);
-   
+
 
     if (err) {
       return res.redirect('/');
     }
-   
+
 
   });
   await db.query(queryNewNote, (err, result) => {
 
-    console.log(result);
-   
+    console.log(result[0]);
+
 
     if (err) {
       return res.redirect('/');
     }
-    newNote = result;
+    newNote = result[0];
+    if (!validateNote(newNote)) {
+      return res.status(500).send('Invalid data type')
+    }
 
+    return res.status(201).send({ newNote })
   });
   // Upon succ, run the following lines to validate the response object and respond to client
   console.log(newNote);
   // --- begin of succ flow ---
-  if (!validateNote(newNote)) {
-    return res.status(500).send('Invalid data type')
-  }
 
-  return res.status(201).send({ newNote })
   // --- end of succ flow ---
 
 
@@ -66,27 +66,21 @@ router.post('/', async (req, res) => {
   // Upon fail, run the following lines to respond with an error
 
   // --- begin of fail flow ---
-  res.status(500).send('Fail to insert')
+  // res.status(500).send('Fail to insert')
   // --- end of fail flow ---
 
 
 
 
 
-  // // TODO-4.1: Remove this section once you start working on TODO-4
-  // // --- Remove section begins ---
-  // const newNote = { id: 2, text: newText, dateCreated: new Date().toISOString().split('T')[0], lastModified: new Date().toISOString().split('T')[0] }
-  // if (!validateNote(newNote)) {
-  //   res.status(500).send('Invalid data type')
-  // }
-  // res.status(201).send({ newNote })
-  // // --- Remove section ends ---
 })
 /* -------------------------------------------------------------------------- */
 
 /* ------------------------- TODO-5 - Update A Note ------------------------- */
-router.put('/', (req, res) => {
+router.put('/', async (req, res) => {
   console.log(`[PUT] http://localhost:${global.port}/note - Updating note`)
+  console.log(`body for note is == ${req.body}`);
+  console.log(`id for note is == ${req.body.id}`);
 
   /*
     TODO-5:
@@ -103,48 +97,62 @@ router.put('/', (req, res) => {
   const noteId = req.body.id
   const newText = req.body.text
 
-  /* 
-
-    // You code here...
-
-    const updatedNote = {} // this is the response object, make sure to replace with actual value
 
 
+  // You code here...
 
-    // Upon succ, run the following lines to validate the response object and respond to client
+  var updatedNote = {} // this is the response object, make sure to replace with actual value
 
-    // --- begin of succ flow ---
-    if (!validateNote(updatedNote)) {
-      res.status(500).send('Invalid data type')
+  let queryUpdateNote = `update notes5 SET text = "${newText}" Where id=${noteId}`;
+
+  let queryGetUpdatedNote = `select * from notes5 Where id=${noteId}`;
+
+   // execute query
+   await db.query(queryUpdateNote, (err, result) => {
+
+    console.log(result);
+    
+    if (err) {
+      return res.status(500).send('Fail to update')
     }
-    res.send({ updatedNote })
-    // --- end of succ flow ---
+    
+
+  });
+
+  await db.query(queryGetUpdatedNote, (err, result) => {
+
+    console.log(result);
+    if(err){
+      return res.status(500).send('Fail to update')
+    }
+    updatedNote = result[0];
+    if (!validateNote(updatedNote)) {
+      return res.status(500).send('Invalid data type')
+    }
+    return res.send({ updatedNote })
+
+  });
+
+  // Upon succ, run the following lines to validate the response object and respond to client
+
+  // --- begin of succ flow ---
+
+  // --- end of succ flow ---
 
 
 
-    // Upon fail, run the following lines to respond with an error
+  // Upon fail, run the following lines to respond with an error
 
-    // --- begin of fail flow ---
-    res.status(500).send('Fail to update')
-    // --- end of fail flow ---
-
-  */
+  // --- begin of fail flow ---
+  // return res.status(500).send('Fail to update')
+  // --- end of fail flow ---
 
 
-
-  // TODO-5.1: Remove this section once you start working on TODO-5
-  // --- Remove section begins ---
-  const updatedNote = { id: noteId, text: newText, dateCreated: '2021-04-15', lastModified: new Date().toISOString().split('T')[0] }
-  if (!validateNote(updatedNote)) {
-    res.status(500).send('Invalid data type')
-  }
-  res.send({ updatedNote })
-  // --- Remove section ends ---
 })
 /* -------------------------------------------------------------------------- */
 
 /* ------------------------- TODO-6 - Delete A Note ------------------------- */
-router.delete('/', (req, res) => {
+router.delete('/', async (req, res) => {
   console.log(`[DELETE] http://localhost:${global.port}/note - Deleting note`)
 
   /*
@@ -156,16 +164,31 @@ router.delete('/', (req, res) => {
   */
   const noteId = req.body.id
 
-  /*
+
 
     // Your code here...
 
+
+  let queryDeleteNoteNote = `delete from notes5 Where id=${noteId}`;
+
+
+   // execute query
+   await db.query(queryDeleteNoteNote, (err, result) => {
+
+
+
+    if (err) {
+      return res.status(500).send('Fail to delete');
+    }
+    return res.send()
+
+  });
 
 
     // Upon succ, run the following lines to validate the response object and respond to client
 
     // --- begin of succ flow ---
-    res.send()
+   
     // --- end of succ flow ---
 
 
@@ -173,17 +196,9 @@ router.delete('/', (req, res) => {
     // Upon fail, run the following lines to respond with an error
 
     // --- begin of fail flow ---
-    res.status(500).send('Fail to delete')
+    // res.status(500).send('Fail to delete')
     // --- end of fail flow ---
 
-  */
-
-
-
-  // TODO-6.1: Remove this section once you start working on TODO-6
-  // --- Remove section begins ---
-  res.send()
-  // --- Remove section ends ---
 })
 /* -------------------------------------------------------------------------- */
 
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { validateNoteArray } = require('../utils/validators')
 
 /* ------------------------ TODO-3 - Fetch All Notes ------------------------ */
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   console.log(`[GET] http://localhost:${global.port}/notes - Fetching all notes`)
 
   /* 
@@ -15,52 +15,30 @@ router.get('/', (req, res) => {
         [{ id, text, dateCreated, lastModified }]
   */
 
-  
-
-    // Your code here...
-    var notes = [];
-    let query = "SELECT * FROM `notes3` ORDER BY id ASC"; // query database to get all the players
-
-    // execute query
-    db.query(query, (err, result) => {
-        if (err) {
-            return res.redirect('/');
-        }
-        notes = result;
-        
-    });
-
-     // this is the response object, make sure to replace with actual value
 
 
+  // Your code here...
+  var notes = [];
+  let query = "SELECT * FROM notes5 ORDER BY id ASC"; // query database to get all the players
 
-    // Upon succ, run the following lines to validate the response object and respond to client
-
-    // --- begin of succ flow ---
+  // execute query
+  await db.query(query, (err, result) => {
+    if (err) {
+      return res.status(500).send('Fail to query');
+    }
+    notes = result;
     if (!validateNoteArray(notes)) {
       return res.status(500).send('Invalid data type')
     }
     return res.send({ notes })
-    // --- end of succ flow ---
+  });
 
 
-
-    // Upon fail, run the following line to respond with an error
-
-    // --- begin of fail flow ---
-    // res.status(500).send('Fail to query')
-    // --- end of fail flow ---
-    
-  
-
-
-
- 
 })
 /* -------------------------------------------------------------------------- */
 
 /* ------------------------- TODO-7 - Search Notes -------------------------- */
-router.get('/search/:searchKey', (req, res) => {
+router.get('/search/:searchKey', async (req, res) => {
   console.log(`[GET] http://localhost:${global.port}/notes/search - Searching notes`)
 
   /*
@@ -76,48 +54,37 @@ router.get('/search/:searchKey', (req, res) => {
   */
   const searchKey = req.params.searchKey
   console.log(searchKey)
- 
-  /*
-
-    // Your code here...
-
-    const notes = [] // this is the response object, make sure to replace with actual value
 
 
-    // Upon succ, run the following lines to validate the response object and respond to client
 
-    // --- begin of succ flow ---
-    if (!validateNoteArray(notes)) {
-      res.status(500).send('Invalid data type')
+  // Your code here...
+
+  var notes = [];// this is the response object, make sure to replace with actual value
+
+  const searchNoteQuery = `SELECT * from notes5 where text like '%${searchKey}%'`
+
+
+  // Upon succ, run the following lines to validate the response object and respond to client
+
+
+  // execute query
+  await db.query(searchNoteQuery, (err, result) => {
+    if (err) {
+      return res.status(500).send('Fail to query')
+
     }
-    res.send({ notes })
-    // --- end of succ flow ---
+    notes = result;
+    if (!validateNoteArray(notes)) {
+      return res.status(500).send('Invalid data type')
+    }
+    return res.send({ notes })
+  });
 
-
-
-    // Upon fail, run the following line to response with error
-
-    // --- begin of fail flow ---
-    res.status(500).send('Fail to query')
-    // --- end of fail flow ---
-    
-  */
-
-
-
-  // TODO-7.1: Remove this line once you start working on TODO-7
-  // --- Remove section begins ---
-  const notes = [ { id: 5, text: `This is a dummy note from search contains search key ${searchKey}!`, dateCreated: '2021-04-15', lastModified: '2021-04-17' } ]
-  if (!validateNoteArray(notes)) {
-    res.status(500).send('Invalid data type')
-  }
-  res.send({ notes })
-  // --- Remove section ends ---
 })
 /* -------------------------------------------------------------------------- */
 
 /* ----------------------- TODO-8 - Delete All Notes ------------------------ */
-router.delete('/', (req, res) => {
+router.delete('/', async (req, res) => {
   console.log(`[DELETE] http://localhost:${global.port}/notes - Deleting all notes`)
 
   /*
@@ -125,34 +92,24 @@ router.delete('/', (req, res) => {
       Delete all notes from the database
   */
 
-  /*
 
-    // Your code here...
-
+  const deleteAllQuery = `delete from notes5`;
 
 
-    // Upon succ, run the following lines to validate the response object and reponse to client
-
-    // --- begin of succ flow ---
-    res.send()
-    // --- end of succ flow ---
+  // Upon succ, run the following lines to validate the response object and respond to client
 
 
+  // execute query
+  await db.query(deleteAllQuery, (err, result) => {
+    if (err) {
+      return res.status(500).send('Fail to delete')
 
-    // Upon fail, run the following line to respond with an error
+    }
 
-    // --- begin of fail flow ---
-    res.status(500).send('Fail to delete')
-    // --- end of fail flow ---
-
-  */
+    return res.send()
+  });
 
 
-
-  // TODO-8.1: Remove this section once you start working on TODO-8
-  // --- Remove section begins ---
-  res.send()
-  // --- Remove section ends ---
 })
 /* -------------------------------------------------------------------------- */
 
